dae0ad2e476e55067308e6408000bb2f37a2f79e
MaxRelekom <61941911+MaxRelekom@users.noreply.github.com>
2024-12-23 13:24:35 +0100

    In production

---
+++ app/ui/ToDoListView.js
@@ -0,0 +1,15 @@
+
+class ToDoListView {
+    
+    /**
+     * Affiche dans chaque sa section dédiée, sa liste de tâches correspondantes
+     * 
+     * @param taskLists: Les listes de tâches 
+     */
+    display(taskLists) {
+        // const todoListSection = document.getElementsByClassName('toDoList-section');
+        console.log(taskLists);
+    }
+}
+
+module.exports = ToDoListView;
--- index.js
+++
@@ -1,5 +0,0 @@
-const ToDoList = require("./app/ToDoList");
-const DATA_PATHS = require("./data/config/dataPaths");
-
-// Créer la ToDoList
-const toDoList = new ToDoList(DATA_PATHS);
